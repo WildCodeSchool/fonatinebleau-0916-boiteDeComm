f72513c5da26c4cb03fe645ddfb90e0180f99f15
Nico <nicolascapron403@gmail.com>
2016-04-13 03:07:22 +0200

    réparé

--- pages/4createurs.php
+++ pages/4createurs.php
@@ -17,37 +17,46 @@
          <div class="feature">
            <div class="feature-picture">
              <img class="staff"src="images/Maud.png" width="200">
-           </div>
-           <p>
-             Choisissez jusque trois parfums
-             parmis notre catalogue de <strong>1000</strong> glaces maison.
+            </div>
+            <p>MAUD JAN-AILLERET</p>
+            <p class="metierscrea"><I>Co-fondatrice</p></I>
+            <p>
+            Mariée, 2 enfants en bas âge (des garçons !), en tenue de consultante, ou de professeur de comm’ le jour ; souvent en costumes sur les planches de théâtre le soir, Maud a une vie bien remplie…
+            Et la communication au sein de son couple pour conjuguer toutes ses activités se doit d’être au top !
            </p>
          </div>
        </div>
        <div class="col-sm-6 col-md-3">
          <div class="feature">
            <img src="images/demas.png" width="185" style="background-image: "images/cadre.png"">
-           <p>
-             L'application vous <strong>géolocalise</strong> automatiquement et vous
-             indique le temps d'attente estimé.
+            <p>MARIE DE CAMAS</p>
+            <p class="metierscrea"><I>Co-fondatrice</p></I>
+            <p>
+            Maman de 3 enfants de 9 à 4 ans, Marie s’est spécialisée dans le marketing et les achats entre Montréal, Shanghai et Oxford.
+            Avec cette vie bien animée et un mari qui voyage beaucoup, Marie se rend compte du sens vital de la communication sous toutes ses formes au sein de son couple.
            </p>
          </div>
        </div>
        <div class="col-sm-6 col-md-3">
          <div class="feature">
            <img src="images/lui.png" width="200">
-           <p>
-             Un de nos supers livreurs fonce pour vous apporter votre glace,
-             qui sera <strong>préparée juste à temps</strong>.
+            <p>GUILLAUME LAGANE</p>
+            <p class="metierscrea"><I>Illustrateur</p></I>
+            <p>
+            C’est un papa très actif à l’emploi du temps surchargé ! 
+            Pour autant, il aime particulièrement retrouver amis et famille le soir venu. Aussi, pour tout mener de front : les séminaires qu’il anime, les bières avec les copains, les biberons à donner et sa femme à gâter,  il utilise tout le temps ses crayons de couleur pour communiquer au mieux !
            </p>
          </div>
        </div>
        <div class="col-sm-6 col-md-3">
          <div class="feature">
            <img src="images/PhotoProfilDo.jpg" width="180" height="230">
-           <p>
-             Pas de monnaie ? Pas un problème, le paiement est <strong>automatiquement</strong>
-             débité sur votre carte bancaire.
+            <p>DOROTHEE CORBIER</p>
+            <p class="metierscrea"><I>Communication</p></I>
+            <p>
+            Dorothée conjugue ses aspirations personnelles avec sa vie professionnelle.
+            Responsable d'une Fondation, c’est une communicante touche-à-tout, au service des projets et causes qu’elle défend.
+            Elle n’en n’oublie pas pour autant son petit bébé et son communicant d’homme – tiens, tiens lui aussi ;-) !
            </p>
          </div>
        </div>

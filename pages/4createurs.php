5c3c0dfb56bca0dad9989ac29431c99d9ef947aa
Nico <nicolascapron403@gmail.com>
2016-04-15 12:09:51 +0200

    pages

--- pages/4createurs.php
+++ pages/4createurs.php
@@ -1,14 +1,13 @@
 <div class="divider" id="section4"></div>
-  <div class="row">
+  <div class="rowequipe">
   <img class="equipeimg" src="images/Equipe.png" alt="equipe" width="35%" height="35%">
-      <div class="container" style="background-color: "> 
+      <div class="container"> 
         <h2 style="font-size: 40px;">
           EQUIPE
         </h2>
       <div class="row"
         <div class="col-lg-12 md-12"></div>
-      </div>
-
+    </div>
 
 
 
@@ -20,7 +19,7 @@
        <div class="col-sm-6 col-md-3">
             <div class="feature">
             <div style="position: relative; left: 0; top: 0;">
-                <img src="images/Maud.png" style="position: relative; top: 0; left: 0;"/>
+                <img class="photomaud" src="images/Maudnoir.png">
                 <img class="photocadre" src="images/cadre.png" width="300" height="380" style="position: absolute; top: 30px; left: 70px;"/>
                 </div>
             <p class="prenom">MAUD JAN-AILLERET</p>
@@ -35,10 +34,10 @@
          <div class="feature">
 
             <img class="photocadre1" src="images/cadre.png" width="280" height="390" style="position: absolute; top: 30px; left: 70px;"/>
-            <img src="images/demas.png" width="185" style="background-image: "images/cadre.png"">
-            <p class="prenom" style="font-family: normal 400 16px/1.4 "europa","Helvetica Neue",Helvetica,sans-serif;">MARIE DE CAMAS</p>
-            <p class="metierscrea" style="font-family: normal 400 16px/1.4 "europa","Helvetica Neue",Helvetica,sans-serif;"><I>Co-fondatrice</p></I>
-            <p class="texte" style="font-family: normal 400 16px/1.4 "europa","Helvetica Neue",Helvetica,sans-serif;">
+            <img src="images/demas.png" width="185">
+            <p class="prenom" style="font-family: normal 400 16px/1.4 ">MARIE DE CAMAS</p>
+            <p class="metierscrea" style="font-family: normal 400 16px/1.4 "><I>Co-fondatrice</p></I>
+            <p class="texte" style="font-family: normal 400 16px/1.4 ">
             Maman de 3 enfants de 9 à 4 ans, Marie s’est spécialisée dans le marketing et les achats entre Montréal, Shanghai et Oxford.
             Avec cette vie bien animée et un mari qui voyage beaucoup, Marie se rend compte du sens vital de la communication sous toutes ses formes au sein de son couple.
            </p>
@@ -48,9 +47,9 @@
          <div class="feature">
          <img class="photocadre2" src="images/cadre.png" width="292" height="400" style="position: absolute; top: 30px; left: 70px;"/>
            <img src="images/lui.png" width="200">
-            <p class="prenom" style="font-family: normal 400 16px/1.4 "europa","Helvetica Neue",Helvetica,sans-serif;">GUILLAUME LAGANE</p>
-            <p class="metierscrea" style="font-family: normal 400 16px/1.4 "europa","Helvetica Neue",Helvetica,sans-serif;"><I>Illustrateur</p></I>
-            <p class="texte" style="font-family: normal 400 16px/1.4 "europa","Helvetica Neue",Helvetica,sans-serif;">
+            <p class="prenom" style="font-family: normal 400 16px/1.4 ">GUILLAUME LAGANE</p>
+            <p class="metierscrea" style="font-family: normal 400 16px/1.4 "><I>Illustrateur</p></I>
+            <p class="texte" style="font-family: normal 400 16px/1.4 ">
             C’est un papa très actif à l’emploi du temps surchargé ! 
             Pour autant, il aime particulièrement retrouver amis et famille le soir venu. Aussi, pour tout mener de front : les séminaires qu’il anime, les bières avec les copains, les biberons à donner et sa femme à gâter,  il utilise tout le temps ses crayons de couleur pour communiquer au mieux !
            </p>
@@ -60,14 +59,15 @@
          <div class="feature">
          <img class="photocadre3" src="images/cadre.png" width="273" height="400" style="position: absolute; top: 30px; left: 70px;"/>
            <img src="images/PhotoProfilDo.jpg" width="180" height="230">
-            <p class="prenom" style="font-family: normal 400 16px/1.4 "europa","Helvetica Neue",Helvetica,sans-serif;">DOROTHEE CORBIER</p>
-            <p class="metierscrea" style="font-family: normal 400 16px/1.4 "europa","Helvetica Neue",Helvetica,sans-serif;"><I>Communication</p></I>
-            <p class="texte" style="font-family: normal 400 16px/1.4 "europa","Helvetica Neue",Helvetica,sans-serif;">
+            <p class="prenom" style="font-family: normal 400 16px/1.4 ">DOROTHEE CORBIER</p>
+            <p class="metierscrea" style="font-family: normal 400 16px/1.4 "><I>Communication</p></I>
+            <p class="texte" style="font-family: normal 400 16px/1.4 ">
             Dorothée conjugue ses aspirations personnelles avec sa vie professionnelle.
             Responsable d'une Fondation, c’est une communicante touche-à-tout, au service des projets et causes qu’elle défend.
             Elle n’en n’oublie pas pour autant son petit bébé et son communicant d’homme – tiens, tiens lui aussi ;-) !
            </p>
-         </div>
-       </div>
-     </div>
-   </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

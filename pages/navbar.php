5cf9a2cf3148e85814c60033104e3e44123457b4
jonathanvalette <jonathanvalette1204@gmail.com>
2016-04-15 08:50:47 +0200

    ppolice modification

--- pages/navbar.php
+++ pages/navbar.php
@@ -1,21 +1,4 @@
-<script>
-$(function() {
-    var pull        = $('#pull');
-        menu        = $('nav ul');
-        menuHeight  = menu.height();
- 
-    $(pull).on('click', function(e) {
-        e.preventDefault();
-        menu.slideToggle();
-    });
-});
-$(window).resize(function(){
-    var w = $(window).width();
-    if(w > 320 && menu.is(':hidden')) {
-        menu.removeAttr('style');
-    }
-}); 
-</script>
+
 <!-- Fixed navbar -->
 <nav id="myNav" class="navbar navbar-inverse" data-spy="affix">
     <div class="container-fluid">

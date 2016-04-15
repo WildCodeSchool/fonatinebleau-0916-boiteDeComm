780e8c97342b536a21e8c2286634568e37b23343
jonathanvalette <jonathanvalette1204@gmail.com>
2016-04-15 11:58:43 +0200

    navbar responsive

--- pages/navbar.php
+++ pages/navbar.php
@@ -1,22 +1,22 @@
 
-<!-- Fixed navbar -->
-<nav id="myNav" class="navbar navbar-inverse" data-spy="affix">
-    <div class="container-fluid">
-        <div class="row nav_bar_row">
-            <div class="col-sm-12">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-                    <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
-                        <span class="icon-bar">qsdqsdq</span>
-                        <span class="icon-bar">qsdqdqsq</span>
-                        <span class="icon-bar">qdqdqdqs</span>
-                        <ul>
-                    </button>
-                </div>
-                <!-- Collection of nav links and other content for toggling -->
-                <div id="navbarCollapse" class="collapse navbar-collapse">
-                    <div class="navbar-collapse collapse" id="navbar-collapsible">
-                        <ul class="nav navbar-nav navbar-left">
+<script>
+$('#nav').affix({
+      offset: {
+        top: $('header').height()
+      }
+});
+</script>
+<!-- Begin Navbar -->
+<nav id="myNav" class="navbar navbar-inverse" data-spy="affix" role="navigation">
+    <div class="container">
+      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+      <a class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="glyphicon glyphicon-bar"></span>
+        <span class="glyphicon glyphicon-bar"></span>
+        <span class="glyphicon glyphicon-bar"></span>
+      </a>
+      <div class="navbar-collapse collapse">
+                            <ul class="nav navbar-nav navbar-left">
                             <li>
                                 <a class="navbar-brand" href="#">
                                     <img id="logo" src="images/logo.png" alt="logo" class="img-circle" alt="#"/>
@@ -29,18 +29,16 @@
                             <li><a style="margin-left:10px;" style="margin-right:15px;" href="#section5">BLOG</a></li>
                             <li><a style="margin-left:10px;" style="margin-right:15px;" href="#section6">CONTACT </a></li>
                         </ul>
-
-                            <button type="button" class="btn btn-lg navbar-right"style="background-color:grey" style="font-color: white"> 
-          						 <span class="glyphicon .glyphicon-shopping-cart"></span>Panier</a>
-
-                            <a class="btn btn-lg navbar-right"style="background-color:grey" style="font-color: white;">
-                                PANIER
-                                <i class="fa fa-shopping-cart" aria-hidden="true" style="size:50px;">
-                                </i>
-                            </a>
-
-                    </div>
-                </div>
-        </div>
+        <ul class="nav pull-right navbar-nav">
+          <li>
+        <button type="button" class="btn btn-lg navbar-right" style="background-color:grey" style="font-color: white"> 
+                                 <span class="glyphicon .glyphicon-shopping-cart"></span>Panier</a>
+         
+      
+            <a href="#"><i class="glyphicon glyphicon-shopping-cart"></i> <span class="badge">2</span></a>
+         </li>
+        </ul>
+      </div>        
     </div>
+  </div><!-- /.navbar -->
 </nav>

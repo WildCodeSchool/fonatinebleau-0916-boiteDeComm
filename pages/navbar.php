e3666df4589e3aa37ee26a0abd8192b7cb2fe6c8
jonathanvalette <jonathanvalette1204@gmail.com>
2016-04-15 13:28:16 +0200

    cha,gement navbar

--- pages/navbar.php
+++ pages/navbar.php
@@ -8,48 +8,35 @@ $('#nav').affix({
 </script>
 <!-- Begin Navbar -->
 <nav id="myNav" class="navbar navbar-inverse" data-spy="affix" role="navigation">
-    <div class="container">
-      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-      <a class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="glyphicon glyphicon-bar"></span>
-        <span class="glyphicon glyphicon-bar"></span>
-        <span class="glyphicon glyphicon-bar"></span>
-      </a>
-      <div class="navbar-collapse collapse">
-                            <ul class="nav navbar-nav navbar-left">
-                            <li>
-                                <a class="navbar-brand" href="#">
-                                    <img id="logo" src="images/logo.png" alt="logo" class="img-circle" alt="#"/>
-                                </a>
-                            </li>
-                            <li><a style="margin-left:10px;" style="margin-right:15px;" href="#section1">PRODUITS</a></li>
-                            <li><a style="margin-left:10px;" style="margin-right:15px;" href="#section2">DEVENIR PARTENAIRE</a></li>
-                            <li><a style="margin-left:10px;" style="margin-right:15px;" href="#section3">OU SOMMES NOUS?</a></li>
-                            <li><a style="margin-left:10px;" style="margin-right:15px;" href="#section4">EQUIPE</a></li>
-                            <li><a style="margin-left:10px;" style="margin-right:15px;" href="#section5">BLOG</a></li>
-                            <li><a style="margin-left:10px;" style="margin-right:15px;" href="#section6">CONTACT </a></li>
-                        </ul>
-<<<<<<< HEAD
-        <ul class="nav pull-right navbar-nav">
-          <li>
-        <button type="button" class="btn btn-lg navbar-right" style="background-color:grey" style="font-color: white"> 
-                                 <span class="glyphicon .glyphicon-shopping-cart"></span>Panier</a>
-         
-      
-            <a href="#"><i class="glyphicon glyphicon-shopping-cart"></i> <span class="badge">2</span></a>
-         </li>
-        </ul>
-      </div>        
-=======
-                            <a class="btn btn-lg navbar-right"style="background-color:grey" style="font-color: white;">
-                                PANIER
-                                <i class="fa fa-shopping-cart" aria-hidden="true" style="size:50px;">
-                                </i>
-                            </a>
-                    </div>
-                </div>
-        </div>
->>>>>>> e2a52824c3a844ab8d8b8a2d7e64a8920f768da8
-    </div>
-  </div><!-- /.navbar -->
+  <div class="container">
+    <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+    <a class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+      <span class="glyphicon glyphicon-bar"></span>
+      <span class="glyphicon glyphicon-bar"></span>
+      <span class="glyphicon glyphicon-bar"></span>
+    </a>
+    <div class="navbar-collapse collapse">
+      <ul class="nav navbar-nav navbar-left">
+        <li>
+            <a class="navbar-brand" href="#">
+                <img id="logo" src="images/logo.png" alt="logo" class="img-circle" alt="#"/>
+            </a>
+        </li>
+        <li><a style="margin-left:10px;" style="margin-right:15px;" href="#section1">PRODUITS</a></li>
+        <li><a style="margin-left:10px;" style="margin-right:15px;" href="#section2">DEVENIR PARTENAIRE</a></li>
+        <li><a style="margin-left:10px;" style="margin-right:15px;" href="#section3">OU SOMMES NOUS?</a></li>
+        <li><a style="margin-left:10px;" style="margin-right:15px;" href="#section4">EQUIPE</a></li>
+        <li><a style="margin-left:10px;" style="margin-right:15px;" href="#section5">BLOG</a></li>
+        <li><a style="margin-left:10px;" style="margin-right:15px;" href="#section6">CONTACT </a></li>
+      </ul> 
+      <ul>
+        <li>
+          <a class="btn btn-lg navbar-right"style="background-color:grey" style="font-color: white;">
+              PANIER
+              <i class="fa fa-shopping-cart" aria-hidden="true" style="size:50px;">
+              </i>
+          </a>
+        </li>
+      </ul>
+  </div>
 </nav>

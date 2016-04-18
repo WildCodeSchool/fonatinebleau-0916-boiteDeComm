3fc870be0bfce5804765f314d0c5cfbc155cb1f1
Florian Grandjean <florian.pdf@gmail.com>
2016-04-19 01:18:42 +0200

    placement des cadres et des commentaires

--- pages/4createurs.php
+++ pages/4createurs.php
@@ -3,64 +3,70 @@
   <!-- Image et titre equipe -->
   <div class="row image_section">
     <img src="images/Equipe.png" alt="equipe"/>
+    <h2> EQUIPE</h2>
   </div>
-  <div> 
-    <h2 class="titreequipe">EQUIPE</h2>
-  </div>
-    <div class="container">
-     <div class="row text-center">
-       <div class="col-sm-6 col-md-3">
-            <div class="feature">
-            <div style="position: relative; left: 0; top: 0;">
-                <img class="photomaud" src="images/Maudnoir.png">
-                <img class="photocadre" src="images/cadre.png" width="300" height="380" style="position: absolute; top: 30px; left: 70px;"/>
-                </div>
-            <p class="prenom">MAUD JAN-AILLERET</p>
-            <p class="metierscrea"><I>Co-fondatrice</p></I>
-            <p class="texte">
-            Mariée, 2 enfants en bas âge (des garçons !), en tenue de consultante, ou de professeur de comm’ le jour ; souvent en costumes sur les planches de théâtre le soir, Maud a une vie bien remplie…
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-6 col-md-3">
+        <div class="feature">
+          <div class="bloc_photo">
+            <img class="photocadre" src="images/cadre.png"/>
+            <img class="img_equipe" src="images/Maudnoir.png"/>
+          </div>
+          <div class="text_createur">
+            <p>MAUD JAN-AILLERET</p>
+            <p><i>Co-fondatrice</i></p>
+            <p>Mariée, 2 enfants en bas âge (des garçons !), en tenue d e consultante, ou de professeur de comm’ le jour ; souvent en costumes sur les planches de théâtre le soir, Maud a une vie bien remplie…
             Et la communication au sein de son couple pour conjuguer toutes ses activités se doit d’être au top !
-           </p>
-         </div>
-       </div>
+            </p>
+          </div>
+        </div>
+      </div>
        <div class="col-sm-6 col-md-3">
          <div class="feature">
-
-            <img class="photocadre1" src="images/cadre.png" width="280" height="390" style="position: absolute; top: 30px; left: 70px;"/>
-            <img src="images/demas.png" width="185">
-            <p class="prenom" style="font-family: normal 400 16px/1.4 ">MARIE DE CAMAS</p>
-            <p class="metierscrea" style="font-family: normal 400 16px/1.4 "><I>Co-fondatrice</p></I>
-            <p class="texte" style="font-family: normal 400 16px/1.4 ">
-            Maman de 3 enfants de 9 à 4 ans, Marie s’est spécialisée dans le marketing et les achats entre Montréal, Shanghai et Oxford.
-            Avec cette vie bien animée et un mari qui voyage beaucoup, Marie se rend compte du sens vital de la communication sous toutes ses formes au sein de son couple.
-           </p>
-         </div>
-       </div>
-       <div class="col-sm-6 col-md-3">
-         <div class="feature">
-         <img class="photocadre2" src="images/cadre.png" width="292" height="400" style="position: absolute; top: 30px; left: 70px;"/>
-           <img src="images/lui.png" width="200">
-            <p class="prenom" style="font-family: normal 400 16px/1.4 ">GUILLAUME LAGANE</p>
-            <p class="metierscrea" style="font-family: normal 400 16px/1.4 "><I>Illustrateur</p></I>
-            <p class="texte" style="font-family: normal 400 16px/1.4 ">
-            C’est un papa très actif à l’emploi du temps surchargé ! 
+          <div class="bloc_photo">
+            <img class="photocadre" src="images/cadre.png"/>
+            <img class="img_equipe" src="images/demas.png">
+          </div>
+          <div class="text_createur">
+            <p>MARIE DE CAMAS</p>
+            <p><I>Co-fondatrice</I></p>
+            <p>Maman de 3 enfants de 9 à 4 ans, Marie s’est spécialisée dans l  e marketing et les achats entre Montréal, Shanghai et Oxford. Avec cette vie bien animée et un mari qui voyage beaucoup, Marie se rend compte du sens vital de la communication sous toutes ses formes au sein de son couple.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6 col-md-3">
+        <div class="feature">
+          <div class="bloc_photo">
+            <img class="photocadre" src="images/cadre.png"/>
+            <img class="img_equipe" src="images/lui.png">
+          </div>
+          <div class="text_createur">
+            <p>GUILLAUME LAGANE</p>
+            <p><i>Illustrateur</i></p>
+            <p>C’est un papa très actif à l’emploi du temps surchargé ! 
             Pour autant, il aime particulièrement retrouver amis et famille le soir venu. Aussi, pour tout mener de front : les séminaires qu’il anime, les bières avec les copains, les biberons à donner et sa femme à gâter,  il utilise tout le temps ses crayons de couleur pour communiquer au mieux !
-           </p>
-         </div>
-       </div>
-       <div class="col-sm-6 col-md-3">
-         <div class="feature">
-         <img class="photocadre3" src="images/cadre.png" width="273" height="400" style="position: absolute; top: 30px; left: 70px;"/>
-           <img src="images/PhotoProfilDo.jpg" width="180" height="230">
-            <p class="prenom" style="font-family: normal 400 16px/1.4 ">DOROTHEE CORBIER</p>
-            <p class="metierscrea" style="font-family: normal 400 16px/1.4 "><I>Communication</p></I>
-            <p class="texte" style="font-family: normal 400 16px/1.4 ">
-            Dorothée conjugue ses aspirations personnelles avec sa vie professionnelle.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6 col-md-3">
+        <div class="feature">
+          <div class="bloc_photo">
+            <img class="photocadre" src="images/cadre.png"/>
+            <img class="img_equipe" src="images/PhotoProfilDo.jpg">
+          </div>
+          <div class="text_createur">
+            <p>DOROTHEE CORBIER</p>
+            <p><i>Communication</i></p>
+            <p>Dorothée conjugue ses aspirations personnelles avec sa vi  e professionnelle.
             Responsable d'une Fondation, c’est une communicante touche-à-tout, au service des projets et causes qu’elle défend.
             Elle n’en n’oublie pas pour autant son petit bébé et son communicant d’homme – tiens, tiens lui aussi ;-) !
-           </p>
-                </div>
-            </div>
+            </p>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </div>

e1dcd1c670484f8939e63b435ec942c978a49d4f
Florian Grandjean <florian.pdf@gmail.com>
2016-04-23 15:19:22 +0200

    index

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php 
 	include('pages/header.php');
 	include('pages/navbar.php');
-	//include('pages/1produits.php');
-	//include('pages/2partenaire.php');
-	//include('pages/3localisation.php');
-	//include('pages/4createurs.php');
-	//include('pages/5blog.php');
-	//include('pages/6contact.php');
+	include('pages/1produits.php');
+	include('pages/2partenaire.php');
+	include('pages/3localisation.php');
+	include('pages/4createurs.php');
+	include('pages/5blog.php');
+	include('pages/6contact.php');
 	include('pages/7footer.php');
 ?>
 
